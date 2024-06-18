01f6650d33ed7bec0f8eb62e60fcb79f10175e2f
Heartzcoder8171 <dashingstar8171>
2024-06-18 12:09:32 +0530

    myfirstcommit

--- Leetcode_Question/Reverse_print.cpp
+++ Leetcode_Question/Reverse_print.cpp
@@ -11,11 +11,11 @@ int reverse(int x){
         x = -x;                            // gives -ve number.
     }
     while(x>0){
-        if(ans>INT8_MAX/10){               // INT_MAX gives the +ve range.
+        if(ans > INT8_MAX / 10){               // INT_MAX gives the +ve range.
             return 0;
         }
         int digit = x % 10;
-        ans = ans * 10 + digit;        // formulae.
+        ans = ans * 10 + digit;        // formulae to reverse the digits.
         x = x/10;
     }
     return isnegative ? -ans : ans;                     // print answer.
